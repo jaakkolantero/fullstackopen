a00dd8c9f8c36fada2e2fe2d23c73274e6ecc1d5
UFo_x <tero.jaakkola1@gmail.com>
2019-10-25 21:55:05 +0300

    3.21 to the internets.

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -131,7 +131,7 @@ app.use(errorHandler);
 
 //module.exports = app;
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
