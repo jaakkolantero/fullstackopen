2701efdc0e011c12c0fb0e169100e06c4163c004
UFo_x <tero.jaakkola1@gmail.com>
2019-10-28 10:45:00 +0200

    feat: 4.5 favoriteBlog funtcion and tests.

--- osa4/blog-backend/tests/list_utils.test.js
+++ osa4/blog-backend/tests/list_utils.test.js
@@ -6,3 +6,95 @@ test("dummy returns one", () => {
   const result = listUtils.dummy(blogs);
   expect(result).toBe(1);
 });
+
+const listWithOneBlog = [
+  {
+    title: "Hello world",
+    author: "Tero Jaakkola",
+    url: "tero.jaakko.la",
+    likes: 1,
+    id: "5db5e743f7f98f0ecc2ee7df"
+  }
+];
+const listWithMultipleBlogs = [
+  {
+    title: "Hello world",
+    author: "Tero Jaakkola",
+    url: "tero.jaakko.la",
+    likes: 2,
+    id: "5db5e743f7f98f0ecc2ee7df"
+  },
+  {
+    title: "Hello world2",
+    author: "Tero Jaakkola",
+    url: "tero.jaakko.la",
+    likes: 5,
+    id: "5db5f13bed5ab314788b507c"
+  },
+  {
+    title: "Hello world3",
+    author: "Tero Jaakkola",
+    url: "tero.jaakko.la",
+    likes: 5,
+    id: "5db69eef47740844f87bec37"
+  }
+];
+const listWithOneFavoriteBlog = [
+  {
+    title: "Hello world",
+    author: "Tero Jaakkola",
+    url: "tero.jaakko.la",
+    likes: 2,
+    id: "5db5e743f7f98f0ecc2ee7df"
+  },
+  {
+    title: "Hello world2",
+    author: "Tero Jaakkola",
+    url: "tero.jaakko.la",
+    likes: 5,
+    id: "5db5f13bed5ab314788b507c"
+  },
+  {
+    title: "Hello world3",
+    author: "Tero Jaakkola",
+    url: "tero.jaakko.la",
+    likes: 4,
+    id: "5db69eef47740844f87bec37"
+  }
+];
+describe("total likes", () => {
+  test("of empty list is zero", () => {
+    const result = listUtils.totalLikes([]);
+    expect(result).toBe(0);
+  });
+  test("when list has only one blog equals the likes of that", () => {
+    const result = listUtils.totalLikes(listWithOneBlog);
+    expect(result).toBe(1);
+  });
+  test("sums multiple blog likes correctly", () => {
+    const result = listUtils.totalLikes(listWithMultipleBlogs);
+    expect(result).toBe(12);
+  });
+});
+
+describe("favorite blog", () => {
+  test("of empty list is zero", () => {
+    const result = listUtils.favoriteBlog([]);
+    expect(result).toStrictEqual([]);
+  });
+  test("when list has only one blog, favorite equals the likes of that", () => {
+    const result = listUtils.favoriteBlog(listWithOneBlog);
+    expect(result).toStrictEqual(listWithOneBlog);
+  });
+  test("return multiple favorite blogs correctly", () => {
+    const result = listUtils.favoriteBlog(listWithMultipleBlogs);
+    expect(result).toStrictEqual([
+      listWithMultipleBlogs[1],
+      listWithMultipleBlogs[2]
+    ]);
+  });
+  test("return one favorite blog correctly", () => {
+    const result = listUtils.favoriteBlog(listWithOneFavoriteBlog);
+    expect(result).toStrictEqual([listWithOneFavoriteBlog[1]]);
+  });
+});
--- osa4/blog-backend/utils/list_utils.js
+++ osa4/blog-backend/utils/list_utils.js
@@ -2,6 +2,19 @@ const dummy = blogs => {
   return 1;
 };
 
+const totalLikes = blogs => {
+  return blogs.reduce(function(total, blog) {
+    return total + blog.likes;
+  }, 0);
+};
+
+const favoriteBlog = blogs => {
+  const maxLikes = Math.max.apply(Math, blogs.map(blog => blog.likes));
+  return blogs.filter(blog => blog.likes === maxLikes);
+};
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 };
