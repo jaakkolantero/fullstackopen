83df9b856f52029e0508ca95df150e6751f716ca
UFo_x <tero.jaakkola1@gmail.com>
2019-10-30 09:58:40 +0200

    feat: 4.8 test and restructure get blogs api.

--- osa4/blog-backend/utils/config.js
+++ osa4/blog-backend/utils/config.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/bloglist";
+const MONGODB_URI =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI || "mongodb://localhost/bloglist";
 const PORT = process.env.PORT || 3003;
 
 module.exports = {
