c3946895c8b329111a2e1ade89a2e20f276d0377
UFo_x <tero.jaakkola1@gmail.com>
2019-10-30 21:38:48 +0200

    feat: 4.18 add token base login

--- osa4/blog-backend/app.js
+++ osa4/blog-backend/app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 const mongoose = require("mongoose");
 const middleware = require("./utils/middleware");
 
@@ -24,6 +25,7 @@ app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
