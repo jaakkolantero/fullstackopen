c185cfcb61e2980379707779eaf8d88bb0a52ee5
UFo_x <tero.jaakkola1@gmail.com>
2019-11-08 10:16:47 +0200

    feat(6.2): implement rest of the app
    '

--- osa6/unicafe-redux/src/index.js
+++ osa6/unicafe-redux/src/index.js
@@ -1,33 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import reducer from './reducer'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import reducer from "./reducer";
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
+  const action = type => {
+    switch (type) {
+      case "GOOD":
+        store.dispatch({
+          type
+        });
+        break;
+      case "OK":
+        store.dispatch({
+          type
+        });
+        break;
+      case "BAD":
+        store.dispatch({
+          type
+        });
+        break;
+      case "ZERO":
+        store.dispatch({
+          type
+        });
+        break;
+
+      default:
+        break;
+    }
+  };
 
   return (
     <div>
-      <button onClick={good}>hyvä</button> 
-      <button>neutraali</button> 
-      <button>huono</button>
-      <button>nollaa tilastot</button>
+      <button onClick={() => action("GOOD")}>hyvä</button>
+      <button onClick={() => action("OK")}>neutraali</button>
+      <button onClick={() => action("BAD")}>huono</button>
+      <button onClick={() => action("ZERO")}>nollaa tilastot</button>
       <div>hyvä {store.getState().good}</div>
-      <div>neutraali</div>
-      <div>huono</div>
+      <div>neutraali {store.getState().ok}</div>
+      <div>huono {store.getState().bad}</div>
     </div>
-  )
-}
+  );
+};
 
 const renderApp = () => {
-  ReactDOM.render(<App />, document.getElementById('root'))
-}
+  ReactDOM.render(<App />, document.getElementById("root"));
+};
 
-renderApp()
-store.subscribe(renderApp)
+renderApp();
+store.subscribe(renderApp);
