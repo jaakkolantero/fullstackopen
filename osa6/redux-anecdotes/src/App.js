cba712ef877fd7aba4736c8ddc763a8ba3f4d689
UFo_x <tero.jaakkola1@gmail.com>
2019-11-08 12:03:01 +0200

    improvement(6.8): move anecdote listing to it's own component

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,26 +1,12 @@
 import React from "react";
-import { vote } from "./reducers/anecdoteReducer";
 import { AnecdoteForm } from "./App/AnecdoteForm";
+import { AnecdoteList } from "./App/AnecdoteList";
 
 const App = ({ store }) => {
-  const anecdotes = store.getState();
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes
-        .sort((a, b) => (a.votes < b.votes ? 1 : b.votes < a.votes ? -1 : 0))
-        .map(anecdote => (
-          <div key={anecdote.id}>
-            <div>{anecdote.content}</div>
-            <div>
-              has {anecdote.votes}
-              <button onClick={() => store.dispatch(vote(anecdote.id))}>
-                vote
-              </button>
-            </div>
-          </div>
-        ))}
+      <AnecdoteList store={store} />
       <h2>create new</h2>
       <AnecdoteForm store={store} />
     </div>
---
+++ osa6/redux-anecdotes/src/App/AnecdoteList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { vote } from "../reducers/anecdoteReducer";
+
+export const AnecdoteList = ({ store }) => {
+  const anecdotes = store.getState();
+  return (
+    <div>
+      {anecdotes
+        .sort((a, b) => (a.votes < b.votes ? 1 : b.votes < a.votes ? -1 : 0))
+        .map(anecdote => (
+          <div key={anecdote.id}>
+            <div>{anecdote.content}</div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => store.dispatch(vote(anecdote.id))}>
+                vote
+              </button>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+};
