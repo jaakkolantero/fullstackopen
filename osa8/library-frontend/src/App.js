b5ec9feee41d09ce469b948d87b830059ccf29ed
UFo_x <tero.jaakkola1@gmail.com>
2019-11-15 20:37:31 +0200

    feat(8.8): implement author page

---
+++ osa8/library-frontend/src/App.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import Authors from "./components/Authors";
+import Books from "./components/Books";
+import NewBook from "./components/NewBook";
+import useSWR from "swr";
+import { request } from "graphql-request";
+
+const API = "http://localhost:4000/graphql";
+
+const App = () => {
+  const [page, setPage] = useState("authors");
+  const { data, error } = useSWR(
+    `{
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }`,
+    query => request(API, query)
+  );
+
+  useEffect(() => {
+    console.log("authors", data);
+    console.log("error", error);
+  }, [data, error]);
+
+  const pages = () => {
+    return (
+      <>
+        <Authors show={page === "authors"} authors={data.allAuthors} />
+        <Books show={page === "books"} />
+        <NewBook show={page === "add"} />
+      </>
+    );
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage("authors")}>authors</button>
+        <button onClick={() => setPage("books")}>books</button>
+        <button onClick={() => setPage("add")}>add book</button>
+      </div>
+
+      {data ? pages() : <div>Loading...</div>}
+    </div>
+  );
+};
+
+export default App;
---
+++ osa8/library-frontend/src/components/Authors.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Authors = ({ show, authors }) => {
+  if (!show) {
+    return null;
+  }
+  return authors ? (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.map(a => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  ) : (
+    <div>Loading authors..</div>
+  );
+};
+
+export default Authors;
